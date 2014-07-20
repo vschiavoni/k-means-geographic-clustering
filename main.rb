aeeeda739547eca8b87bd6e52db4dc4c84c5b10c
Valerio Schiavoni <valerio.schiavoni@gmail.com>
2014-07-20 18:22:08 +0200

    remove mongodb dep

--- main.rb
+++ main.rb
@@ -1,49 +1,23 @@
 require 'rubygems'
-require 'mongo'
-
+require "csv"
 require_relative 'lib/kmeans'
 require_relative 'lib/geometric'
-
 include Geometric
-
-def random_coordinate(range)
-  min, max = range
-  rand * (max - min) + min
-end
-
-db = Mongo::Connection.new('localhost').db('clustering')
-coll = db['points']
-
-# Insert 100 randomly-generated locations into a MongoDB database
-if not coll.find.count > 0
-
-  for i in 1..100
-    # Generate a point located roughly in downtown Toronto
-    longitude_range = [-79.45, -79.30]
-    latitude_range = [43.65, 43.70]
-
-    lon = random_coordinate(longitude_range)
-    lat = random_coordinate(latitude_range)
-
-    doc = {'coordinates' => [lon,lat]}
-    coll.insert(doc, :safe => true)
-  end
-
-  puts 'Inserted points into database.'
-
-end
-
-# Create the index for querying
-coll.ensure_index([['coordinates', Mongo::GEO2D]])
-puts 'Ensured 2D index for coordinate points.'
-
-# Cluster one level using k-means
 points = []
-coll.find().each do |point|
-  node = Kmeans::Node.new(point['coordinates'])
-  points << node
-end
+rng=Random.new(1234) #fixed seed
 
-clusters = Kmeans::cluster(points, 10)
-p clusters
+CSV.foreach("it-2004.sites.gpscoords.csv") do |row|
+    #lat, long = row
+    #puts "#{row}"
+    node = Kmeans::Node.new(row,rng)
+    points << node
+end
+k=ARGV[0].to_i or 4
+puts "KMeans, k=#{k}"
+clusters = Kmeans::cluster(points,k,rng)
+#p clusters
 puts "#{clusters.length} clusters created successfully."
+
+#clusters.each {|c| 
+#  c.nodes.each{|n| puts n}
+#}
